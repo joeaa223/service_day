30ad3ea86ef6b57a25112cbb000826c8e340c5da
Dam Chong <damchong95@gmail.com>
2026-02-06 14:54:53 +0800

    Update Notification Service

--- apps/notifications/services.py
+++ apps/notifications/services.py
@@ -1,9 +1,22 @@
 from django.contrib.auth.models import User
 from .models import Notification
 
-def notify_all_users(title, message, url):
-    users = User.objects.all()
 
+def notify_user(user, title, message, url):
+    """
+    Send notification to a single user
+    """
+    Notification.objects.create(
+        user=user,
+        title=title,
+        message=message,
+        url=url
+    )
+
+def notify_users(users, title, message, url):
+    """
+    Send notification to a list/queryset of users
+    """
     notifications = [
         Notification(
             user=user,
@@ -13,5 +26,12 @@ def notify_all_users(title, message, url):
         )
         for user in users
     ]
-
     Notification.objects.bulk_create(notifications)
+
+
+def notify_all_users(title, message, url):
+    """
+    Broadcast notification to all users
+    """
+    users = User.objects.all()
+    notify_users(users, title, message, url)
